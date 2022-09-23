0b5f0548a2c9401c0af96ca13732fbea61b6f6bd
Levi Milli <107660704+LeviMilli@users.noreply.github.com>
2022-09-23 09:06:45 -0700

    on iteration 4

--- routes/drones.js
+++ routes/drones.js
@@ -1,21 +1,46 @@
 const express = require('express');
 const router = express.Router();
 
+let DroneModel = require('../models/Drone.model')
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
+  DroneModel.find()
+  .then((drone) => {
+    res.render('list.hbs', {drone})
+    console.log(response)
+  })
+  .catch((err) => {
+    console.log('Some error in finding', err)
+  })
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  res.render('drones/create-form.hbs')
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  console.log(req.body)
+  DroneModel.create(req.body)
+  .then(() => {
+    
+    res.redirect('/drones') // redirects to a URL
+  })
+  .catch((err) => {
+    res.render('drones/create-form.hbs')
+    console.log('Some error in finding', err)
+  })
+});
+
+
+router.get('/drones/:id', (req, res, next) => {
+  res.render('drones/descriptions.hbs')
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,19 @@
-// Iteration #1
+// Connect to your DB
+require('../db')
+const mongoose = require('mongoose')
+
+// Add data
+let DroneModel = require('../models/Drone.model')
+DroneModel.create([
+    {name: 'DroneX', propellers: 3, maxSpeed: 23},
+    {name: 'DroneBig', propellers: 7, maxSpeed: 30},
+    {name: 'DroneSmall', propellers: 1, maxSpeed: 15}
+])
+    .then(() => {
+        console.log('Data added')
+        mongoose.connection.close();
+    })
+    .catch(() => {
+        console.log('Error while adding data')
+    })
+
