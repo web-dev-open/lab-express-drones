6e0d085637c9a9b7fd9fc2b5984055e0a058fc72
chathurahasanga44 <s92067130@ousl.lk>
2024-01-13 14:21:18 +0530

    done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,13 @@
 // Iteration #1
+// Drone.model.js
+const mongoose = require('mongoose');
+
+const droneSchema = new mongoose.Schema({
+  name: String,
+  propellers: Number,
+  maxSpeed: Number,
+});
+
+const Drone = mongoose.model('Drone', droneSchema);
+
+module.exports = Drone;
