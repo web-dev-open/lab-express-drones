7fc854fdda4da8ca04bf88e60d24727296f0c21d
Shashank519915 <120128150+Shashank519915@users.noreply.github.com>
2024-02-04 16:18:15 +0530

    Update drones.seed.js

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,26 @@
 // Iteration #1
+require('../db');
+
+
+const mongoose = require('mongoose');
+const droneModel = require('../models/Drone.model');
+
+// Updated drone data
+const dronesData = [
+  { name: "Phantom X3", propellers: 4, maxSpeed: 15 },
+  
+  { name: "SkyRunner 2000", propellers: 6, maxSpeed: 22 },
+  
+  { name: "Stealth Ninja", propellers: 8, maxSpeed: 25 }
+];
+
+droneModel.create(dronesData)
+  .then((result) => {
+    console.log('Success!');
+  })
+  .catch((err) => {
+    console.log('Failed!', err);
+  })
+  .finally(() => {
+    mongoose.connection.close();
+  });
