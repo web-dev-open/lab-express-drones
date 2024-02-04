1b0773dd6730547f92afff77d9e8972ca6b09064
Shashank519915 <120128150+Shashank519915@users.noreply.github.com>
2024-02-04 16:15:21 +0530

    Update Drone.model.js

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,12 @@
 // Iteration #1
+const {Schema, model} = require('mongoose');
+
+const droneSchema = new Schema( {
+    name: String,
+    propellers: Number,
+    maxSpeed: Number,
+});
+
+const drone = model("drones", droneSchema);
+
+module.exports = drone;
